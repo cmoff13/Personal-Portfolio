13d054ac7117032c4ebe0b5da02e98f694a96c44
cmoff13 <60751534+cmoff13@users.noreply.github.com>
2020-03-02 13:19:35 -0700

    Add files via upload

---
+++ starwars.js
@@ -0,0 +1,30 @@
+import { films } from '../../data/films.js'
+import { people } from '../../data/people.js'
+
+console.log("Hi there! My first JavaScript Code")
+
+console.log(document.querySelector('.greeting'))
+
+let greetingDiv = document.querySelector('.greeting')
+
+greetingDiv.textContent = "Star Wars Cast"
+
+let castList = document.createElement("ul")
+
+let counter = 1
+
+people.forEach(person => {
+    let nameItem = document.createElement("li")
+    nameItem.textContent = person.name
+    castList.appendChild(nameItem)
+    let personImg = document.createElement("img")
+    personImg.src = `https://starwars-visualguide.com./assets/img/characters/${counter}.jpg`
+    greetingDiv.appendChild(personImg)
+    counter++
+})
+
+greetingDiv.appendChild(castList)
+
+
+
+
