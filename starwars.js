7711242f48a2e7bfeb85e9259e8fcd9c4384679d
cmoff13 <60751534+cmoff13@users.noreply.github.com>
2020-03-04 12:21:05 -0700

    Add files via upload

--- starwars.js
+++ starwars.js
@@ -3,7 +3,6 @@ import { people } from '../../data/people.js'
 
 console.log("Hi there! My first JavaScript Code")
 
-console.log(document.querySelector('.greeting'))
 
 let greetingDiv = document.querySelector('.greeting')
 
@@ -11,19 +10,55 @@ greetingDiv.textContent = "Star Wars Cast"
 
 let castList = document.createElement("ul")
 
-let counter = 1
+const maleCharacters = people.filter(person => person.gender === 'male')
+console.log(maleCharacters)
 
-people.forEach(person => {
-    let nameItem = document.createElement("li")
-    nameItem.textContent = person.name
-    castList.appendChild(nameItem)
-    let personImg = document.createElement("img")
-    personImg.src = `https://starwars-visualguide.com./assets/img/characters/${counter}.jpg`
-    greetingDiv.appendChild(personImg)
-    counter++
+const femaleCharacters = people.filter(person => person.gender === 'female')
+console.log(femaleCharacters)
+
+const otherCharacters = people.filter(person => person.gender === "n/a" || person.gender === "none" || person.gender === "hermaphrodite")
+console.log(otherCharacters)
+
+let maleButton = document.querySelector('#maleButton')
+let femaleButton = document.querySelector('#femaleButton')
+let otherButton = document.querySelector('#otherButton')
+
+maleButton.addEventListener('click', function(event) {
+    populateDOM(maleCharacters)
 })
 
-greetingDiv.appendChild(castList)
+femaleButton.addEventListener('click', function(event) {
+    populateDOM(femaleCharacters)
+})
+
+otherButton.addEventListener('click', function(event) {
+    populateDOM(otherCharacters)
+})
+
+let counter = 1
+
+function populateDOM(characters) {
+    
+    characters.forEach(person => {
+        console.log(`${person.url}`)
+    let personAnchor = document.createElement("a")
+    personAnchor.href = "#"
+    let personImg = document.createElement("img")
+    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    
+    personImg.addEventListener("error", (event) => { 
+        personImg.hidden = true
+    })
+
+    personImg.addEventListener("click", function( event ) { 
+        console.log("Thanks for clicking!")
+    })
+
+    personAnchor.appendChild(personImg)
+    greetingDiv.appendChild(personAnchor)
+    counter++
+})
+}
 
 
 
